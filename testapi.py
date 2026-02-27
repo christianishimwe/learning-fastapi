6400a677d4f87f1d4cc77ee4d33b739757e454dd
Christian Ishimwe <christianishimwe2003@gmail.com>
2026-02-27 14:42:16 -0500

    feat: 4

--- testapi.py
+++ testapi.py
@@ -3,9 +3,9 @@ import requests
 BASE_URL = "https://learning-fastapi-258839900414.europe-west1.run.app"
 
 # Test the root endpoint
-r = requests.get(f"{BASE_URL}/")
-print(f"Root endpoint: {r.json()}")
+#r = requests.get(f"{BASE_URL}/")
+#print(f"Root endpoint: {r.json()}")
 
 #r = requests.post(f"{BASE_URL}/user", json={"id": 5, "name": "Christian"})
-r = requests.get(f"{BASE_URL}/items/")
+r = requests.get(f"{BASE_URL}/identity/Ch")
 print(r.json())
